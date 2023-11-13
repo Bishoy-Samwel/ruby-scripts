3896854095a1d1f94dc96269e3a0ef13dc32346e
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-11-13 17:24:02 +0200

    add choices fto read or to write

--- food-calc.rb
+++ food-calc.rb
@@ -1,65 +1,81 @@
-# Initialize total cost and units hash
-total_cost = 0
+# Initialize units hash
 units = {}
 
-loop do
-  # Get the unit name from the user
-  puts "Enter the unit name: "
-  unit_name = gets.chomp
-  puts "The unit name: #{unit_name}"
-  puts ''
+puts "Do you want to read from the file or create new entries? (read/new)"
+action = gets.chomp.downcase
 
-  # Get the unit weight from the user
-  puts "Enter the unit weight: "
-  unit_weight = gets.chomp.to_f
-  puts "The unit weight: #{unit_weight}"
-  puts ''
+if action == 'read'
+  # Open the file for reading
+  File.open('units.txt', 'r') do |file|
+    # Read each line from the file
+    file.each_line do |line|
+      # Parse the unit details
+      unit_name, weight, price, daily_amount = line.chomp.split(',')
 
-  # Get the unit price from the user
-  puts "Enter the unit price: "
-  unit_price = gets.chomp.to_f
-  puts "The unit price: #{unit_price}"
-  puts ''
+      # Store the unit details
+      units[unit_name] = {
+        weight: weight.to_f,
+        price: price.to_f,
+        daily_amount: daily_amount.to_f
+      }
+    end
+  end
 
-  # Get the amount taken daily from the user
-  puts "Enter the amount taken daily: "
-  amount_taken_daily = gets.chomp.to_f
-  puts "The amount taken daily: #{amount_taken_daily}"
-  puts ''
+  # Initialize total cost
+  total_cost = 0
 
-  # Calculate the daily price
-  daily_price = (unit_price * amount_taken_daily) / unit_weight 
+  # Calculate the total cost
+  units.each do |unit_name, details|
+    daily_price = (details[:price] * details[:daily_amount]) / details[:weight]
+    total_cost += daily_price
+  end
 
-  # Add to total cost
-  total_cost += daily_price
+  # Print the total cost
+  puts "The total daily price for all units is #{total_cost}"
 
-  # Store the unit and its daily price
-  units[unit_name] = daily_price
+else
+  loop do
+    # Get the unit details from the user
+    puts "Enter the unit name: "
+    unit_name = gets.chomp
+    puts "The unit name: #{unit_name}"
+    puts ''
 
-  # Print the result
-  puts "The daily price for this unit is #{daily_price}"
-  puts "The total daily price so far is #{total_cost}"
-  puts ''
+    puts "Enter the unit weight: "
+    unit_weight = gets.chomp.to_f
+    puts "The unit weight: #{unit_weight}"
+    puts ''
 
-  # Ask user if they want to add another unit
-  puts "Do you want to add another unit? (yes/no)"
-  answer = gets.chomp.downcase
+    puts "Enter the unit price: "
+    unit_price = gets.chomp.to_f
+    puts "The unit price: #{unit_price}"
+    puts ''
 
-  # Break the loop if the answer is 'no'
-  break if answer != 'yes'
-end
+    puts "Enter the amount taken daily: "
+    amount_taken_daily = gets.chomp.to_f
+    puts "The amount taken daily: #{amount_taken_daily}"
+    puts ''
 
-# Print the total cost
-puts "The total daily price for all units is #{total_cost}"
+    # Store the unit details
+    units[unit_name] = {
+      weight: unit_weight,
+      price: unit_price,
+      daily_amount: amount_taken_daily
+    }
 
-# Open a file for writing
-File.open('units.txt', 'w') do |file|
-  # Write the total cost to the file
-  file.puts "The total daily price for all units is #{total_cost}"
+    # Ask user if they want to add another unit
+    puts "Do you want to add another unit? (yes/no)"
+    answer = gets.chomp.downcase
 
-  # Print and write each unit and its daily price
-  units.each do |unit_name, daily_price|
-    puts "The daily price for #{unit_name} is #{daily_price}"
-    file.puts "The daily price for #{unit_name} is #{daily_price}"
+    # Break the loop if the answer is 'no'
+    break if answer != 'yes'
+  end
+
+  # Open a file for writing
+  File.open('units.txt', 'w') do |file|
+    # Write each unit and its details to the file
+    units.each do |unit_name, details|
+      file.puts "#{unit_name},#{details[:weight]},#{details[:price]},#{details[:daily_amount]}"
+    end
   end
 end
