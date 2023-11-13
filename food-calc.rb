b0ddba3c7c36d07ddcb58f5bf8600b8f83fa5161
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-11-13 17:25:13 +0200

    add seperate functions

--- food-calc.rb
+++ food-calc.rb
@@ -1,18 +1,9 @@
-# Initialize units hash
-units = {}
-
-puts "Do you want to read from the file or create new entries? (read/new)"
-action = gets.chomp.downcase
-
-if action == 'read'
-  # Open the file for reading
-  File.open('units.txt', 'r') do |file|
-    # Read each line from the file
+# Stores the units and their details in a hash
+def read_units_from_file(filename)
+  units = {}
+  File.open(filename, 'r') do |file|
     file.each_line do |line|
-      # Parse the unit details
       unit_name, weight, price, daily_amount = line.chomp.split(',')
-
-      # Store the unit details
       units[unit_name] = {
         weight: weight.to_f,
         price: price.to_f,
@@ -20,62 +11,61 @@ if action == 'read'
       }
     end
   end
+  units
+end
 
-  # Initialize total cost
-  total_cost = 0
-
-  # Calculate the total cost
+# Displays the units and their details
+def display_units(units)
   units.each do |unit_name, details|
-    daily_price = (details[:price] * details[:daily_amount]) / details[:weight]
-    total_cost += daily_price
+    puts "Unit name: #{unit_name}, Weight: #{details[:weight]}, Price: #{details[:price]}, Daily amount: #{details[:daily_amount]}"
   end
+end
 
-  # Print the total cost
-  puts "The total daily price for all units is #{total_cost}"
+# Gets the unit details from the user and returns them as a hash
+def get_unit_details_from_user
+  puts "Enter the unit name: "
+  unit_name = gets.chomp
+  puts "Enter the unit weight: "
+  unit_weight = gets.chomp.to_f
+  puts "Enter the unit price: "
+  unit_price = gets.chomp.to_f
+  puts "Enter the amount taken daily: "
+  daily_amount = gets.chomp.to_f
+  { unit_name => { weight: unit_weight, price: unit_price, daily_amount: daily_amount } }
+end
 
-else
-  loop do
-    # Get the unit details from the user
-    puts "Enter the unit name: "
-    unit_name = gets.chomp
-    puts "The unit name: #{unit_name}"
-    puts ''
-
-    puts "Enter the unit weight: "
-    unit_weight = gets.chomp.to_f
-    puts "The unit weight: #{unit_weight}"
-    puts ''
-
-    puts "Enter the unit price: "
-    unit_price = gets.chomp.to_f
-    puts "The unit price: #{unit_price}"
-    puts ''
-
-    puts "Enter the amount taken daily: "
-    amount_taken_daily = gets.chomp.to_f
-    puts "The amount taken daily: #{amount_taken_daily}"
-    puts ''
-
-    # Store the unit details
-    units[unit_name] = {
-      weight: unit_weight,
-      price: unit_price,
-      daily_amount: amount_taken_daily
-    }
-
-    # Ask user if they want to add another unit
-    puts "Do you want to add another unit? (yes/no)"
-    answer = gets.chomp.downcase
-
-    # Break the loop if the answer is 'no'
-    break if answer != 'yes'
-  end
-
-  # Open a file for writing
-  File.open('units.txt', 'w') do |file|
-    # Write each unit and its details to the file
+# Writes the units and their details to a file
+def write_units_to_file(units, filename)
+  File.open(filename, 'w') do |file|
     units.each do |unit_name, details|
       file.puts "#{unit_name},#{details[:weight]},#{details[:price]},#{details[:daily_amount]}"
     end
   end
 end
+
+# Main script
+def main_script
+  puts "Do you want to read from the file or create new entries? (read/new)"
+  action = gets.chomp.downcase
+
+  units = action == 'read' ? read_units_from_file('units.txt') : {}
+
+  display_units(units)
+
+  if action == 'read'
+    puts "Do you want to add more units? (yes/no)"
+    answer = gets.chomp.downcase
+    return if answer != 'yes'
+  end
+
+  loop do
+    units.merge!(get_unit_details_from_user)
+    puts "Do you want to add another unit? (yes/no)"
+    break if gets.chomp.downcase != 'yes'
+  end
+
+  write_units_to_file(units, 'units.txt')
+end
+
+# Run the script
+main_script
