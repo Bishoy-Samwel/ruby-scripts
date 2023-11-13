cd12eb598dd64b1af08401cc9cc32854314f3d9e
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-11-13 21:59:44 +0200

    add: full daily price

--- food-calc.rb
+++ food-calc.rb
@@ -5,11 +5,12 @@ def read_units_from_file(filename)
   units = {}
   File.open(filename, 'r') do |file|
     file.each_line do |line|
-      unit_name, weight, price, daily_amount = line.chomp.split(',')
+      unit_name, weight, price, daily_amount, daily_price = line.chomp.split(',')
       units[unit_name] = {
         weight: weight.to_f,
         price: price.to_f,
-        daily_amount: daily_amount.to_f
+        daily_amount: daily_amount.to_f,
+        daily_price: daily_price.to_f
       }
     end
   end
@@ -19,7 +20,7 @@ end
 # Displays the units and their details
 def display_units(units)
   units.each do |unit_name, details|
-    puts "Unit name: #{unit_name}, Weight: #{details[:weight]}, Price: #{details[:price]}, Daily amount: #{details[:daily_amount]}"
+    puts "Unit name: #{unit_name}, Weight: #{details[:weight]}, Price: #{details[:price]}, Daily amount: #{details[:daily_amount]}, daily_price: #{details[:daily_price]}"
   end
 end
 
@@ -33,28 +34,45 @@ def get_unit_details_from_user
   unit_price = gets.chomp.to_f
   puts 'Enter the amount taken daily: '
   daily_amount = gets.chomp.to_f
-  { unit_name => { weight: unit_weight, price: unit_price, daily_amount: daily_amount } }
+  daily_price = ((unit_price.to_f * daily_amount.to_f) / unit_weight.to_f).ceil
+  puts "The daily price: #{daily_price}"
+
+  { unit_name => { weight: unit_weight, price: unit_price, daily_amount: daily_amount, daily_price: daily_price } }
 end
 
 # Writes the units and their details to a file
 def write_units_to_file(units, filename)
   File.open(filename, 'w') do |file|
     units.each do |unit_name, details|
-      file.puts "#{unit_name},#{details[:weight]},#{details[:price]},#{details[:daily_amount]}"
+      file.puts "#{unit_name},#{details[:weight]},#{details[:price]},#{details[:daily_amount]}, #{details[:daily_price]}"
     end
   end
 end
 
+# Calculates the full price for the units
+def calculate_full_price(units)
+  total_price = 0.0
+  units.each do |_unit_name, details|
+    total_price += details[:daily_price]
+  end
+  total_price
+end
+
 # Main script
 def main_script
+  puts "\nWelcome to our units manager!\nYou can either read existing units from a file or create new ones."
   puts 'Do you want to read from the file or create new entries? (read/new)'
   action = gets.chomp.downcase
+  units = {}
 
-  units = action == 'read' ? read_units_from_file('units.txt') : {}
+  if action == 'read'
+    units = read_units_from_file('units.txt')
+  end
 
   display_units(units)
 
   if action == 'read'
+    puts "The full price: #{calculate_full_price(units)}"
     puts 'Do you want to add more units? (yes/no)'
     answer = gets.chomp.downcase
     return if answer != 'yes'
@@ -63,7 +81,10 @@ def main_script
   loop do
     units.merge!(get_unit_details_from_user)
     puts 'Do you want to add another unit? (yes/no)'
-    break if gets.chomp.downcase != 'yes'
+    if gets.chomp.downcase != 'yes'
+      puts "The full price: #{calculate_full_price(units)}"
+      break
+    end
   end
 
   write_units_to_file(units, 'units.txt')
